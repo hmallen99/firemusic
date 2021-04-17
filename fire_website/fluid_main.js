d842063c5375f1e3bd2ed154177318b667ed593b
hmallen99 <henmanallen@berkeley.edu>
2021-04-16 22:47:49 -0700

    added velocity

--- fire_website/fluid_main.js
+++ fire_website/fluid_main.js
@@ -19,15 +19,20 @@ let renderer,
     scene,
     camera,
     material,
+    alternateMaterial,
     volconfig,
     cmtextures,
     object,
     container,
     fireMesh,
+    alternateFireMesh,
     textureShader,
-    textureMapVariable,
     outputRenderTarget,
+    alternateRenderTarget,
     velocityMap0,
+    texture0,
+    advectVariable,
+    advectUniforms,
     gpuCompute;
 
 init();
@@ -45,9 +50,6 @@ function init() {
     scene = new THREE.Scene();
 
     // Probably don't need this
-    const sun = new THREE.DirectionalLight(0xFFFFFF, 1.0);
-    sun.position.set(300, 400, 175);
-    scene.add(sun)
 
     renderer = new THREE.WebGLRenderer();
     renderer.setPixelRatio(window.devicePixelRatio);
@@ -61,7 +63,18 @@ function init() {
 
 function init_fire() {
     const geometry = new THREE.PlaneGeometry(WIDTH, WIDTH, WIDTH, WIDTH);
-    const material = new THREE.ShaderMaterial({
+    material = new THREE.ShaderMaterial({
+        uniforms: THREE.UniformsUtils.merge( [
+            THREE.ShaderLib['phong'].uniforms, 
+            {
+                map: {value: null},
+            }
+        ]),
+        vertexShader: document.getElementById('vertexShader').textContent,
+        fragmentShader: document.getElementById('fragmentShader').textContent
+    });
+
+    alternateMaterial = new THREE.ShaderMaterial({
         uniforms: THREE.UniformsUtils.merge( [
             THREE.ShaderLib['phong'].uniforms, 
             {
@@ -73,6 +86,7 @@ function init_fire() {
     });
 
     fireMesh = new THREE.Mesh(geometry, material);
+    alternateFireMesh = new THREE.Mesh(geometry, alternateMaterial);
     scene.add(fireMesh);
 
     // create a 2d texture that encodes 3d data, e.g. the texture below is a 3 x 2 x 6 cube
@@ -91,10 +105,6 @@ function init_fire() {
     //const divergenceMap0 = gpuCompute.createTexture();
 
     gpuCompute = new GPUComputationRenderer(WIDTH, WIDTH, renderer);
-    const texture0 = gpuCompute.createTexture();
-
-    
-    
     /*var p = 0;
     for (var j = 0; j < 8; j++) {
         for (var i = 0; i < 8; i++) {
@@ -113,32 +123,61 @@ function init_fire() {
     }*/
 
     
-    velocityMap0 = gpuCompute.createTexture();
-    const pixels = velocityMap0.image.data;
+    var velocityMap = gpuCompute.createTexture();
+    const pixels = velocityMap.image.data;
     var p = 0;
     for (var i = 0; i < WIDTH; i++) {
         for (var j = 0; j < WIDTH; j++) {
-            pixels[p] = i / WIDTH;
-            pixels[p + 1] = j / WIDTH;
+            pixels[p] = 0.0;
+            pixels[p + 1] = 0.0;
             pixels[p + 2] = 0.0;
             pixels[p + 3] = 1.0;
             p += 4;
         }
     }
 
-    textureShader = gpuCompute.createShaderMaterial(document.getElementById('textureShader').textContent, {
+
+    /*texture0 = gpuCompute.createTexture();
+    const pixels2 = velocityMap0.image.data;
+    p = 0;
+    for (var i = 0; i < WIDTH; i++) {
+        for (var j = 0; j < WIDTH; j++) {
+            pixels2[p] = 0.0;
+            pixels2[p + 1] = 0.0;
+            pixels2[p + 2] = 0.0;
+            pixels2[p + 3] = 1.0;
+            p += 4;
+        }
+    }*/
+
+    advectVariable = gpuCompute.addVariable("advectVariable", document.getElementById("advectShader").textContent, velocityMap);
+    gpuCompute.setVariableDependencies(advectVariable, [advectVariable]);
+    advectUniforms = advectVariable.material.uniforms;
+    advectUniforms['timestep'] = {value: 0.01};
+    advectUniforms['velocitySampler'] = {value: null};
+
+    advectVariable.wrapS = THREE.RepeatWrapping;
+    advectVariable.wrapT = THREE.RepeatWrapping;
+
+    //gpuCompute.setVariableDependencies(advectVariable, [advectVariable]);
+
+    const error = gpuCompute.init();
+    if (error != null) {
+        console.log(error);
+    }
+
+    /*textureShader = gpuCompute.createShaderMaterial(document.getElementById('advectShader').textContent, {
         textureSampler: {value: null},
-    });
-    outputRenderTarget = gpuCompute.createRenderTarget();
+        timestep: {value: 0.5},
+    });*/
     
-    textureShader.uniforms.textureSampler.value = velocityMap0;
-    
-    material.uniforms.map.value = outputRenderTarget.texture;
+    //outputRenderTarget = gpuCompute.createRenderTarget();
 
-    gpuCompute.doRenderTarget(textureShader, outputRenderTarget);
-
-    
-
+    //textureShader.uniforms.textureSampler.value = texture0;
+    //textureShader.uniforms.textureSampler.value = outputRenderTarget.texture;
+    //gpuCompute.doRenderTarget(textureShader, outputRenderTarget);   
+    //material.uniforms.map.value = gpuCompute.getCurrentRenderTarget(advectVariable).texture;
+    //material.uniforms.map.value = outputRenderTarget.texture;
 }
 
 function onWindowResize() {
@@ -150,14 +189,26 @@ function onWindowResize() {
 
 function animate() {
     requestAnimationFrame(animate);
-
+    update();
     render();
 }
 
-function render () {
-    
+function update() {
+    let advectTexture = gpuCompute.getCurrentRenderTarget(advectVariable).texture;
+    material.uniforms.map.value = advectTexture;
+
+    let advectAltTexture = gpuCompute.getAlternateRenderTarget(advectVariable).texture;
+    alternateMaterial.uniforms.map.value = advectAltTexture;
+
+    //advectUniforms['timestep'].value = ((advectUniforms['timestep'].value * 100) % 100) / 100 + 0.01;
+    advectUniforms['velocitySampler'].value = advectTexture;
 
     gpuCompute.compute();
+}
+
+
+function render () {
+    
     renderer.render(scene, camera);
 }
 
