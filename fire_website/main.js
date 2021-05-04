a29ae3fcd14de487fc9f2cb3cafc1c4cffafbfe1
Priyanka Kargupta <pkargupta@berkeley.edu>
2021-05-04 13:42:19 -0400

    forgot one space

--- fire_website/main.js
+++ fire_website/main.js
@@ -653,11 +653,13 @@ class EmberSystem {
   _onKeyUp(event) {
     // TODO: switch this to the Demo class
     switch(event.keyCode) {
+    /*
       case 32: // SPACE
       this._particles = [];
       this._stop = !this._stop;
         //this._AddParticles();
         break;
+    */
     }
   }
 
