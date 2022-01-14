1167c8ecebc75e382de7c481eb1b0cb4ee74309a
hmallen99 <henmanallen@berkeley.edu>
2022-01-14 00:03:28 -0800

    Begin main.js decomp

--- fire_website/main.js
+++ fire_website/main.js
@@ -10,70 +10,7 @@ import {
 	emberFragmentShader
 } from './shaders/particleShaders.js';
 
-// Spotify API Integration:
-async function authorize(){
-	let myHeaders = new Headers();
-	var my_clientID = '03bbc4ece6d945009d5607c6958b26ac';
-	var clientSecret = 'ecbaf00f9fd947ecac83a873e6a7e014';
-	myHeaders.append("Authorization", 'Basic ' + btoa(my_clientID + ':' + clientSecret));
-	myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-	var urlencoded = new URLSearchParams();
-	urlencoded.append("grant_type", "client_credentials");
-
-	const requestOptions = {
-	method: 'POST',
-	headers: myHeaders,
-	body: urlencoded,
-	redirect: 'follow'
-	}
-
-	let res = await fetch("https://accounts.spotify.com/api/token", requestOptions);
-	res = await res.json();
-	return res.access_token;
-}
-
-async function search(title, art_name){
-	const access_token = await authorize();
-	//this.setState({access_token});
-	const BASE_URL = 'https://api.spotify.com/v1/search';
-	var track = 'track:' + title;
-	var artist = 'artist:' + art_name;
-	if (artist == 'artist:') {
-		artist = '';
-	}
-	if (track == 'track:') {
-		track = '';
-	}
-	let FETCH_URL = BASE_URL + '?q=' + track + '%20' + artist  + '&type=track&limit=1';
-	//const ALBUM_URL = 'https://api.spotify.com/v1/artists';
-
-	let myHeaders = new Headers();
-	myHeaders.append("Authorization", `Bearer ${access_token}`);
-
-	const requestOptions = {
-		method: 'GET',
-		headers: myHeaders
-	}
-
-	let res = await fetch(FETCH_URL, requestOptions);
-	res = await res.json();
-	let id = res.tracks.items[0].id;
-
-	const BASE_URL_T = 'https://api.spotify.com/v1/audio-features/';
-	let FETCH_URL_T = BASE_URL_T + id;
-
-	myHeaders = new Headers();
-	myHeaders.append("Authorization", `Bearer ${access_token}`);
-
-	const requestOptions_T = {
-		method: 'GET',
-		headers: myHeaders
-	}
-
-	let res_T = await fetch(FETCH_URL_T, requestOptions_T);
-	res_T = await res_T.json();
-	return res_T;
-}
+import { search } from './spotifyAPI.js';
 
 var dataArray;
 var analyser;
@@ -119,39 +56,7 @@ var vizInit = function (){
 window.onload = vizInit();
 
 // Particle Implementation
-class LinearSpline {
-  constructor(lerp) {
-    this._points = [];
-    this._lerp = lerp;
-  }
-
-  AddPoint(t, d) {
-    this._points.push([t, d]);
-  }
-
-  Get(t) {
-    let p1 = 0;
-
-    for (let i = 0; i < this._points.length; i++) {
-      if (this._points[i][0] >= t) {
-        break;
-      }
-      p1 = i;
-    }
-
-    const p2 = Math.min(this._points.length - 1, p1 + 1);
-
-    if (p1 == p2) {
-      return this._points[p1][1];
-    }
-
-    return this._lerp(
-        (t - this._points[p1][0]) / (
-            this._points[p2][0] - this._points[p1][0]),
-        this._points[p1][1], this._points[p2][1]);
-  }
-}
-
+import LinearSpline from './LinearSpline.js';
 
 class ParticleSystem {
   constructor(params) {
